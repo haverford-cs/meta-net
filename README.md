a15d2609d823227fe8af33810d53d3c0457c018c
Gareth Nicholas <gareth.enigma@gmail.com>
2019-12-08 22:27:53 -0500

    Normalization complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # meta-net
-Formerly meta-net, now a meta analysis of using models to classify images of flowers. 
+Formerly meta-net, now classifying German street signs using convolutional
+neural networks!
+
+## Work Log
+12/8/2019: Gareth (4 Hours) 
+  1) Wrote code to load in the training and testing data using cv2, csv, and
+  numpy
+  2) Wrote code to normalize the image data and convert it to 32 x 32
--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ import numpy as np
 
 
 training_path = "Training/Final_Training/Images/"
-testing_path = "Testing/Final_Test/"
+testing_path = "Testing/Final_Test/Images"
 
 def load_training_data():
     """Read in the training data.
@@ -43,18 +43,65 @@ def load_training_data():
             for row in info_reader:
                 # Path to one image. Slice off around the boundaries of
                 # the sign.
-                image = cv2.imread(os.path.join(one_label_dir,row[0]))
+                image = cv2.imread(os.path.join(one_label_dir, row[0]))
                 image = image[np.int(row[4]):np.int(row[6]),
                             np.int(row[3]):np.int(row[5]), :]
                 label = np.int(row[7])
                 data.append(image)
                 labels.append(label)
-            info_reader.close()
+            csv_file.close()
+    data = normalize(data)
     return data, labels
 
-def normalize(img_data):
+def load_testing_data():
+    """Read in the testing data.
+
+    Returns
+    -------
+    data
+        List of cv2 images.
+    labels
+        Integer label of each cv2 image.
+
+    """
+    data = []
+    labels = []
+    csv_name = "GT-final_test.csv"
+    csv_path = os.path.join(testing_path, csv_name)
+    csv_file = open(csv_path, "r")
+    # Again, not actually comma separated...
+    info_reader = csv.reader(csv_file, delimiter = ";")
+    # Skip the header again
+    next(info_reader)
+    for row in info_reader:
+        image = cv2.imread(os.path.join(testing_path, row[0]))
+        image = image[np.int(row[4]):np.int(row[6]),
+                    np.int(row[3]):np.int(row[5]), :]
+        label = np.int(row[7])
+        data.append(image)
+        labels.append(label)
+    csv_file.close()
+    data = normalize(data)
+    return data, labels
 
 
+def normalize(data):
+    """Resize images to 32x32 then normalize.
 
-if __name__ == "__main__":
-    load_training_data()
+    Parameters
+    ----------
+    data : list
+        List of images.
+
+    Returns
+    -------
+    Numpy array
+        Normalized images.
+
+    """
+    data = [cv2.resize(image, (32, 32)) for image in data]
+    data = np.array(data).astype(np.float32)
+    mean_pixel = data.mean(axis = (0, 1, 2), keepdims = True)
+    std_pixel = data.std(axis=(0, 1, 2), keepdims = True)
+    data = (data - mean_pixel) / std_pixel
+    return data
