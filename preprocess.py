412acf883db9a442b5a8c84b03586f7d282cb4c9
Gareth Nicholas <gareth.enigma@gmail.com>
2019-12-08 21:04:22 -0500

    Update gitignore

---
+++ preprocess.py
@@ -0,0 +1,7 @@
+"""
+Utilities for preprocessing the German Traffic Sign Dataset. The data we are
+working with contains images of various sizes and can be found at
+http://benchmark.ini.rub.de/?section=gtsrb&subsection=dataset
+Authors: Gareth Nicholas + Emile Givental 
+Date: December 8th, 2019
+"""
